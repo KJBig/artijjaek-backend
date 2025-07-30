7a076fb93d0860448b17ddbec0ffcde0cf780faf
KIM JUNGI <101792740+KJBig@users.noreply.github.com>
2025-07-30 15:55:48 +0900

    - admin 모듈에 종속성 삭제 및 추가.

--- noati-admin/build.gradle.kts
+++ noati-admin/build.gradle.kts
@@ -3,6 +3,5 @@ plugins {
 
 dependencies {
     implementation(project(":noati-core"))
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    runtimeOnly("com.mysql:mysql-connector-j")
+    implementation("org.springframework.boot:spring-boot-starter-web")
 }
