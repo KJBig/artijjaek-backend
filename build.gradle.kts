e295cdc97706f4b317fa4bf8297823357c0a296e
KIM JUNGI <101792740+KJBig@users.noreply.github.com>
2025-08-07 13:18:49 +0900

    feat : QueryDSL 설정 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
+    kotlin("kapt") version "1.9.25" apply false
     id("org.springframework.boot") version "3.5.4"
     id("io.spring.dependency-management") version "1.1.7"
 }
