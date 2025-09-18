912e37ee1cbf909e328e05b23d3a8a2fe2e6f375
KIM JUNGI <101792740+KJBig@users.noreply.github.com>
2025-09-18 11:03:56 +0900

    [#18] fix : EnableScheduling 어노테이션 위치 변경
    - 각 스케줄러 클래스 -> 스프링 어플리케이션 클래스.

--- artiting-app/src/main/kotlin/com/artiting/app/ArtitingApplication.kt
+++ artiting-app/src/main/kotlin/com/artiting/app/ArtitingApplication.kt
@@ -2,7 +2,9 @@ package com.artiting.app
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
+@EnableScheduling
 @SpringBootApplication(scanBasePackages = ["com.artiting.admin", "com.artiting.api", "com.artiting.batch", "com.artiting.core"])
 class ArtitingApplication
 
