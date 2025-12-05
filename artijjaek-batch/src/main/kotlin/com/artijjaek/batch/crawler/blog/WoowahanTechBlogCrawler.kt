072aaff2d21a70abc40b1a9257b1b65af3b63069
KJBig <101792740+KJBig@users.noreply.github.com>
2025-12-06 02:18:53 +0900

    feat : 우아한형제들 기술 블로그 크롤러 추가

---
+++ artijjaek-batch/src/main/kotlin/com/artijjaek/batch/crawler/blog/WoowahanTechBlogCrawler.kt
@@ -0,0 +1,17 @@
+package com.artijjaek.batch.crawler.blog
+
+import com.artijjaek.core.domain.company.entity.Company
+import org.jsoup.nodes.Document
+import org.springframework.stereotype.Component
+
+@Component
+class WoowahanTechBlogCrawler(
+) : RssCrawler() {
+
+    override val blogName: String = "WOOWAHAN TECH"
+
+    override fun findFirstImageElement(htmlDoc: Document, company: Company): String? {
+        return null
+    }
+
+}
