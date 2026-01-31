2b779a8828d1cb6d2d5bd65af4addcfd435651af
KJBig <101792740+KJBig@users.noreply.github.com>
2026-02-01 02:15:53 +0900

    [#115] feat : 어드민 Entity 생성
    - Admin Entity 생성
    - AdminRole Enum 생성

---
+++ artijjaek-core/src/main/kotlin/com/artijjaek/core/domain/admin/entity/Admin.kt
@@ -0,0 +1,28 @@
+package com.artijjaek.core.domain.admin.entity
+
+import com.artijjaek.core.common.entity.BaseEntity
+import com.artijjaek.core.domain.admin.enums.AdminRole
+import jakarta.persistence.*
+
+@Entity
+class Admin(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "admin_id")
+    var id: Long? = null,
+
+    @Column(nullable = false)
+    var name: String,
+
+    @Column(nullable = true)
+    var email: String,
+
+    @Column(nullable = true)
+    var password: String,
+
+    @Column(nullable = true)
+    var adminRole: AdminRole,
+
+    ) : BaseEntity() {
+}
