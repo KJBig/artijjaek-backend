55941cf763a8cbb83172bcf83e00a85bb9f4e833
KJBig <101792740+KJBig@users.noreply.github.com>
2026-02-04 22:45:51 +0900

    [#118] feat : Member 엔티티 관련 테스트 코드 추가
    - core 모듈에 테스트 코드 추가

---
+++ artijjaek-core/src/test/kotlin/com/artijjaek/core/member/entity/MemberTest.kt
@@ -0,0 +1,29 @@
+package com.artijjaek.core.member.entity
+
+import com.artijjaek.core.domain.member.entity.Member
+import com.artijjaek.core.domain.member.enums.MemberStatus
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import kotlin.test.Test
+
+class MemberTest {
+
+    @Test
+    @DisplayName("사용자의 상태를 변경할 수 있다")
+    fun changeMemberStatusTest() {
+        // given
+        val member = Member(
+            email = "test@example.com",
+            nickname = "nickname",
+            uuidToken = "some-uuid-token",
+            memberStatus = MemberStatus.ACTIVE
+        )
+
+        // when
+        member.changeMemberStatus(MemberStatus.DELETED)
+
+        // then
+        assertThat(member.memberStatus).isEqualTo(MemberStatus.DELETED)
+    }
+
+}
