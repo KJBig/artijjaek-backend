c8d1dcbc791ef8af01d0c05ac7fbcacdc0ae4dbe
KJBig <101792740+KJBig@users.noreply.github.com>
2026-02-16 00:34:03 +0900

    feat : member 관련 테스트 코드 추가

--- artijjaek-core/src/test/kotlin/com/artijjaek/core/domain/member/entity/MemberTest.kt
+++ artijjaek-core/src/test/kotlin/com/artijjaek/core/domain/member/entity/MemberTest.kt
@@ -25,4 +25,40 @@ class MemberTest {
         assertThat(member.memberStatus).isEqualTo(MemberStatus.DELETED)
     }
 
+    @Test
+    @DisplayName("사용자의 이메일을 변경할 수 있다")
+    fun changeEmailTest() {
+        // given
+        val member = Member(
+            email = "test@example.com",
+            nickname = "nickname",
+            uuidToken = "some-uuid-token",
+            memberStatus = MemberStatus.ACTIVE
+        )
+
+        // when
+        member.changeEmail(null)
+
+        // then
+        assertThat(member.email).isNull()
+    }
+
+    @Test
+    @DisplayName("사용자의 닉네임을 변경할 수 있다")
+    fun changeNicknameTest() {
+        // given
+        val member = Member(
+            email = "test@example.com",
+            nickname = "nickname",
+            uuidToken = "some-uuid-token",
+            memberStatus = MemberStatus.ACTIVE
+        )
+
+        // when
+        member.changeNickname("nick")
+
+        // then
+        assertThat(member.nickname).isEqualTo("nick")
+    }
+
 }
