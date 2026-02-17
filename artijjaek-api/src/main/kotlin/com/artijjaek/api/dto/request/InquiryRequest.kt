4ba1f7b0d48ff466486fe2a2ca18da2bfcaba661
KJBig <101792740+KJBig@users.noreply.github.com>
2026-02-17 17:45:05 +0900

    feat : 문의하기 입력 폼 검증 추가

--- artijjaek-api/src/main/kotlin/com/artijjaek/api/dto/request/InquiryRequest.kt
+++ artijjaek-api/src/main/kotlin/com/artijjaek/api/dto/request/InquiryRequest.kt
@@ -1,6 +1,14 @@
 package com.artijjaek.api.dto.request
 
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.Size
+
 data class InquiryRequest(
+    @field:Email
+    @field:Size(max = 255, message = "이메일이 최대 길이를 넘었습니다.")
     val email: String,
+
+    @field:NotEmpty(message = "내용을 입력하세요.")
     val content: String,
 )
